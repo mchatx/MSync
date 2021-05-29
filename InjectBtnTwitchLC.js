200785227b0f178a27b7d114b6740876f3d493d6
Cendekia Abdi <mikatzukiathanas@gmail.com>
2021-05-30 00:54:35 +0900

    Filter works for popout too

---
+++ InjectBtnTwitchLC.js
@@ -0,0 +1,416 @@
+var UID = document.location.toString().substring(document.location.toString().indexOf("/popout/") + 8);
+UID = UID.substring(0, UID.indexOf("/chat"));
+UID = "Twitch " + UID;
+
+//-------------------------------------   LISTEN TO LIVE CHAT BOX   -----------------------------------------
+//   READING LIVE CHAT BOX NODE CHANGE
+const callback = function(mutationsList, observer) {
+    for(const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+			mutation.addedNodes.forEach(element => {
+				var author = element.getElementsByClassName("chat-author__display-name")[0].textContent;
+				var TextMsg = element.getElementsByClassName("text-fragment")[0].textContent;
+				data = {                            
+					"Act": 'MChad-Entry',
+					"UID": UID,
+					"Tag": author,
+					"Stime": '',
+					"Stext": '',
+					"CC": '',
+					"OC": ''
+				};
+				data.Stext = "[" + author + "] " + TextMsg;
+
+				if (Live){
+				} else {
+					data.Stime = TimespanStringify(element.innerText.split("\n")[0]);
+				}					
+
+				if(element.getElementsByClassName("chat-badge") != null){
+					var Chatbadges = element.getElementsByClassName("chat-badge");
+
+					if (Chatbadges.length == 0){
+						if ((TagList.length == 0) && (KeyWordList.length == 0)){
+							ws.send(JSON.stringify(data));
+						} else if ((FilterMessageTag(author)) || FilterMessageKeyword(TextMsg)) {
+							ws.send(JSON.stringify(data));
+						}
+						return;
+					}
+
+					for (var i = 0; i < Chatbadges.length; i++){
+						if(Chatbadges[i].getAttribute("alt") == "Moderator"){
+							data.Tag += ",moderator";
+							ws.send(JSON.stringify(data));
+							break;
+						} else if (i == Chatbadges.length - 1){
+							if ((TagList.length == 0) && (KeyWordList.length == 0)){
+								ws.send(JSON.stringify(data));
+							} else if ((FilterMessageTag(author)) || FilterMessageKeyword(TextMsg)) {
+								ws.send(JSON.stringify(data));
+							}
+						}
+					}
+				} else {
+					if ((TagList.length == 0) && (KeyWordList.length == 0)){
+						ws.send(JSON.stringify(data));
+					} else if ((FilterMessageTag(author)) || FilterMessageKeyword(TextMsg)) {
+						ws.send(JSON.stringify(data));
+					}
+				}
+				//console.log(JSON.stringify(data))
+				// SEND THEM TO APP OR FILTER FIRST BEFORE SENDING
+			});
+        }
+    }
+}
+
+const config = { childList: true, subtree: false };
+const ChatItemObserver = new MutationObserver(callback);
+var VidDur;
+
+function ChatListener(){
+	if (Live){
+	} else {
+		var Chatbox = document.getElementsByClassName("simplebar-content");
+
+		for (var i = 0; i < Chatbox.length; i++){
+			if (Chatbox[i].className == "simplebar-content"){
+				ListenerTarget = Chatbox[i].childNodes[0];
+				ChatItemObserver.observe(ListenerTarget, config);
+				break;
+			} else if (i == Chatbox.length - 1){
+				ws.close();
+				spn.textContent = "Can't find Chat Box";
+			}
+		}
+	}
+}
+
+function TimespanStringify(timechange){
+
+	switch (timechange.split(":").length){
+		case (1):
+			if (timechange.length == 1){
+				timechange = "0" + timechange;
+			}
+			timechange = "00:00:" + timechange;
+			break;
+		case (2):
+			if (timechange.length == 4){
+				timechange = "0" + timechange;
+			}
+			timechange = "00:" + timechange;
+			break;
+		case (3):
+			if (timechange.length == 7){
+				timechange = "0" + timechange;
+			}
+			break;
+	}
+	
+	return (timechange);
+}
+
+function TimespanStringify2(timechange){
+	var mn, hr;
+	timechange = timechange.toString().split(".")[0];
+	mn = Math.floor(timechange/60);
+	timechange -= mn*60;
+	hr = Math.floor(mn/60);
+	mn -= hr * 60;
+	
+	if (timechange.toString().length == 1) timechange = "0" + timechange.toString();
+	if (mn.toString().length == 1) mn = "0" + mn.toString();
+	if (hr.toString().length == 1) hr = "0" + hr.toString();
+	return (hr + ":" + mn + ":" + timechange);
+}
+//=============================================================================================================
+
+
+
+//-------------------------------------------- FILTER UTIL  ---------------------------------------------------
+var TagList = "";
+var KeyWordList = "";
+
+function FilterMessageTag(author){
+	if (TagList.includes(author)) {
+		return (true);
+	} else {
+		return (false);
+	}
+}
+
+function FilterMessageKeyword(text){
+	if (KeyWordList.length != 0){
+		var Res = text.match(new RegExp(KeyWordList, "i"));
+		if (Res == null){
+			return(false);
+		}
+
+		if (Res.length != 0){
+			return (true);
+		} else {
+			return (false);
+		}
+	} else {
+		return (false);
+	}
+}
+
+//=============================================================================================================
+
+
+
+//--------------------------------------------- BASIC UTIL ----------------------------------------------------
+function SendReg(){
+    var data = {                           
+		"Act": 'MChad-Reg',
+		"UID": UID,
+		"Nick": UID
+	};	
+	ws.send(JSON.stringify(data));
+}
+
+function SendRollCall(){
+    var data = {                           
+		"Act": 'MChad-RollCall',
+		"UID": UID,
+		"Nick": title.textContent.substring(0, title.textContent.lastIndexOf("#")).trim()
+	};	
+	ws.send(JSON.stringify(data));
+}
+
+function SendUnsync(){
+	var data = {                           
+		"Act": 'MChad-Unsync',
+		"UID": UID
+	};
+	ws.send(JSON.stringify(data));
+}
+
+function OpenSync() {
+	if (document.location.toString().indexOf("://www.twitch.tv/") != -1){
+		ws = new WebSocket("ws://localhost:20083/"); //	This one is fixed, host 2008 for Mio's birthday 20 Aug and 3 for "Mi" in Mio
+		//ws.onerror = function (err) {
+		//}
+
+		ws.onopen = function (event) {
+			SendReg();
+		};
+        
+        ws.onclose = function (event) {
+			switch (mode){
+				case 1:
+					spn.textContent = "Can't reach server";
+					break;
+				case 3:
+					spn.textContent = "Disconnected";
+					break;
+				case 4:
+					sendBtn = null;
+					ChatText = null;
+					ChatInputPanel = null;
+					ChatBoxObserver.disconnect();
+					break;
+				case 5:
+					ChatItemObserver.disconnect();
+					break;
+				case 6:
+					spn.textContent = "Disconnected";
+					break;	
+			}
+			btn.textContent = "Sync MChat";
+			mode = 0;
+        };
+		
+		ws.onmessage = function (event) {
+			MsgNexus(event.data.toString());
+		};
+	
+	} else {
+		alert('NOT IN SYNCING TARGET WEBPAGE');
+	}
+}
+
+function MsgNexus(StringData) {
+	var NexusParse = StringData.toString().match(/\"Act\":\"MChad-RegOK\"|\"Act\":\"MChad-RollCallApp\"|\"Act\":\"MChad-SetMode\"|\"Act\":\"MChad-PlayApp\"|\"Act\":\"MChad-PauseApp\"|\"Act\":\"MChad-TimeSetApp\"|\"Act\":\"MChad-LiveSend\"|\"Act\":\"MChad-RegListener\"|\"Act\":\"MChad-FilterApp\"|\"Act\":\"MChad-PreciseSyncApp\"|\"Act\":\"MChad-PreciseTimeSetApp\"/);
+	
+	if (NexusParse == null){
+		return;
+	}
+
+	if (NexusParse.length != 0){
+		switch(NexusParse[0]){
+			case ("\"Act\":\"MChad-RegOK\""):
+				mode = 2;
+				btn.textContent = "Synced - Idle";
+				break;
+			case ("\"Act\":\"MChad-RollCallApp\""):
+				SendRollCall();
+				break;
+			case ("\"Act\":\"MChad-SetMode\""):
+				var ParsedData = StringData.toString().split("\",\"");
+				if (ParsedData.length != 3){
+					return;
+				}
+		
+				if (ParsedData[1].split("\":\"")[1].replace("\"","") == UID){
+					switch (ParsedData[2].split("\":\"")[1].replace("\"}","")){
+						case ("Archive"):
+                                mode = 2;
+                                btn.textContent = "PRECISE SYNC MODE UNAVAILABLE";
+							break;
+						case ("LiveChat"):
+							if (mode < 3){
+								mode = 2;
+								btn.textContent = "BOUNCING NOT AVAILABLE";
+							}
+							break;
+
+						case ("SyncTL"):
+							if (mode < 3){
+								mode = 2;
+								btn.textContent = "PRECISE SYNC MODE UNAVAILABLE";
+							}
+							break;
+	
+					}
+				}
+				break;
+			case ("\"Act\":\"MChad-TimeSetApp\""):
+				var ParsedData = StringData.toString().split("\",\"");
+				if (ParsedData.length != 3){
+					return;
+				}
+		
+				if ((ParsedData[1].split("\":\"")[1].replace("\"","") == UID) && (mode == 3)){
+					SetTime(ParsedData[2].split("\":\"")[1].replace("\"}",""));
+				}
+				break;
+			case ("\"Act\":\"MChad-LiveSend\""):
+				var ParsedData = StringData.toString().split("\",\"");
+				if (ParsedData.length != 3){
+					return;
+				}
+
+				if ((ParsedData[1].split("\":\"")[1].replace("\"","") == UID) && (mode == 4)){
+					SendTextEnter(ParsedData[2].split("\":\"")[1].replace("\"}",""));
+				}
+				break;
+			case ("\"Act\":\"MChad-RegListener\""):
+				if (mode < 3){
+					mode = 5;
+					btn.textContent = "Synced - Listener (Click to Unsync)";
+					ChatListener();
+				}
+				break;
+			case ("\"Act\":\"MChad-FilterApp\""):
+				var ParsedData = StringData.toString().split("\",\"");
+				if (ParsedData.length != 3){
+					return;
+				}
+				
+				switch (ParsedData[1].split("\":\"")[1].replace("\"","")){
+					case ("TAG"):
+						TagList = ParsedData[2].split("\":\"")[1].replace("\"}","").replaceAll(",", " ");
+						break;
+					case ("KEYWORD"):
+						KeyWordList = ParsedData[2].split("\":\"")[1].replace("\"}","");
+						break;						
+				}
+				spn.textContent = "Synced Filter"
+				break;
+			case ("\"Act\":\"MChad-PreciseTimeSetApp\""):
+				var ParsedData = StringData.toString().split("\",\"");
+				if (ParsedData.length != 4){
+					return;
+				}
+							
+				if ((ParsedData[1].split("\":\"")[1].replace("\"}","") == UID) && (mode == 6)){
+					SetTimePrecise(ParsedData[2].split("\":\"")[1], ParsedData[3].split("\":\"")[1].replace("\"}",""));
+				}				
+				break;
+
+		}
+	}
+}
+
+function BtnNexus() {
+	spn.textContent = "";
+	if (mode == 0) {
+		btn.textContent = "Syncing..."
+		mode = 1;
+		OpenSync();
+	} else {
+		ws.close();
+	}
+}
+//=============================================================================================================
+
+
+
+var ws;
+var btn = document.createElement('button');
+btn.onclick = BtnNexus;
+btn.textContent = "Sync MChat"
+btn.style.margin = "5px"
+btn.style.background = 'black';
+btn.style.color = 'white';
+btn.style.fontSize = '15px';
+btn.style.cursor = 'pointer';
+btn.style.textAlign = 'center';
+btn.style.borderRadius = '15px';
+btn.style.padding = '8px';
+
+var spn = document.createElement('span');
+spn.textContent = "";
+spn.style.fontSize = '15px';
+spn.style.background = 'black';
+spn.style.color = 'white';
+
+var sendBtn; 
+var ChatText;
+var ListenerTarget;
+var ChatInputPanel;
+
+var Live = false;
+var mode = 0;
+
+var ChatElementTarget = "chat-room-header-label";
+/*
+	0 : NOT SYNCED
+	1 : SYNCING
+	2 : SYNCED-IDLE
+	3 : SYNCED-ARCHIVE
+	4 : SYNCED-LIVECHAT
+	5 : SYNCED-LISTENER
+*/
+
+function LoadButtons() {
+	var target = document.getElementById(ChatElementTarget).parentNode.parentNode.parentNode;
+	var ExtContainer = document.createElement('div');
+	ExtContainer.id = "Extcontainer";
+	target.prepend(ExtContainer);
+	ExtContainer.appendChild(btn);
+	ExtContainer.appendChild(spn);
+}
+
+async function WaitUntilLoad(){
+	while (true){
+		var target = document.getElementById(ChatElementTarget);
+		if (target != null){
+			if (document.getElementById("Extcontainer") != null){
+				var Extcontainer = document.getElementById("Extcontainer");
+				Extcontainer.parentNode.removeChild(Extcontainer);
+			}
+			LoadButtons();
+			break;
+		} else {
+			console.log("Not loaded yet");
+		}
+		await new Promise(resolve => setTimeout(resolve, 1000));
+	}
+}
+
+WaitUntilLoad();
---
+++ InjectBtnYoutubeLC.js
@@ -0,0 +1,370 @@
+var UID = document.location.toString().substring(document.location.toString().indexOf("v=") + 2);
+if (UID.indexOf("?") != -1){
+	UID = UID.substring(0, UID.indexOf("?"));
+}
+if (UID.indexOf("&") != -1){
+	UID = UID.substring(0, UID.indexOf("&"));
+}
+UID = "Youtube " + UID;
+
+//-------------------------------------   LISTEN TO LIVE CHAT BOX   -----------------------------------------
+//   READING LIVE CHAT BOX NODE CHANGE
+const callback = function(mutationsList, observer) {
+    for(const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+			mutation.addedNodes.forEach(element => {
+				data = {                            
+					"Act": 'MChad-Entry',
+					"UID": UID,
+					"Tag": element.innerText.split("\n")[0],
+					"Stime": '',
+					"Stext": '',
+					"CC": '',
+					"OC": ''
+				};
+
+				data.Stime = "";
+				data.Stext = "[" + element.innerText.split("\n")[0] + "] " + element.innerText.split("\n")[1];
+				
+				if (element.getAttribute("author-type") != null) {
+					var tagscan = element.getAttribute("author-type").toLowerCase().match(/mod|moderator|owner/);
+					if (tagscan != null){
+						if (tagscan.length != 0){
+							data.Tag += "," + element.getAttribute("author-type");
+							ws.send(JSON.stringify(data));
+							return true;
+						}
+					}
+				} 
+				
+				if ((TagList.length == 0) && (KeyWordList.length == 0)){
+					ws.send(JSON.stringify(data));
+				} else if ((FilterMessageTag(element.innerText.split("\n")[0])) || FilterMessageKeyword(element.innerText.split("\n")[1])) {
+					ws.send(JSON.stringify(data));
+				}
+
+				//console.log(JSON.stringify(data))
+				// SEND THEM TO APP OR FILTER FIRST BEFORE SENDING
+			});
+        }
+    }
+}
+
+const config = { childList: true, subtree: false };
+const config2 = { attributes: true, childList: false, subtree: false };
+const ChatItemObserver = new MutationObserver(callback);
+
+function ChatListener(){
+    var target = document.getElementsByClassName("style-scope yt-live-chat-item-list-renderer");
+    for (let j = 0; j < target.length; j++){
+        if (target[j].id == "items"){
+                ListenerTarget = target[j];
+                ChatItemObserver.observe(ListenerTarget, config);
+            break;
+        } else if (j == target.length - 1){
+            ws.close();
+            spn.textContent = "Can't find Chat Box";
+        }
+    }
+}
+
+function TimespanStringify(timechange){
+	var mn, hr;
+	timechange = timechange.toString().split(".")[0];
+	mn = Math.floor(timechange/60);
+	timechange -= mn*60;
+	hr = Math.floor(mn/60);
+	mn -= hr * 60;
+	
+	if (timechange.toString().length == 1) timechange = "0" + timechange.toString();
+	if (mn.toString().length == 1) mn = "0" + mn.toString();
+	if (hr.toString().length == 1) hr = "0" + hr.toString();
+	return (hr + ":" + mn + ":" + timechange);
+}
+//=============================================================================================================
+
+
+
+//-------------------------------------------- FILTER UTIL  ---------------------------------------------------
+var TagList = "";
+var KeyWordList = "";
+
+function FilterMessageTag(author){
+	if (TagList.includes(author)) {
+		return (true);
+	} else {
+		return (false);
+	}
+}
+
+function FilterMessageKeyword(text){
+	if (KeyWordList.length != 0){
+		var Res = text.match(new RegExp(KeyWordList, "i"));
+		if (Res == null){
+			return(false);
+		}
+
+		if (Res.length != 0){
+			return (true);
+		} else {
+			return (false);
+		}
+	} else {
+		return (false);
+	}
+}
+
+//=============================================================================================================
+
+
+
+//--------------------------------------------- BASIC UTIL ----------------------------------------------------
+function SendReg(){
+    var data = {                           
+		"Act": 'MChad-Reg',
+		"UID": UID,
+		"Nick": UID
+	};	
+	ws.send(JSON.stringify(data));
+}
+
+function SendRollCall(){
+    var data = {                           
+		"Act": 'MChad-RollCall',
+		"UID": UID,
+		"Nick": title[0].textContent.replaceAll("】", "]").replaceAll("【", "[").replaceAll("～", "-")
+	};	
+	ws.send(JSON.stringify(data));
+}
+
+function SendUnsync(){
+	var data = {                           
+		"Act": 'MChad-Unsync',
+		"UID": UID
+	};
+	ws.send(JSON.stringify(data));
+}
+
+function OpenSync() {
+	if (document.location.toString().indexOf("https://www.youtube.com/live_chat?") != -1){
+		ws = new WebSocket("ws://localhost:20083/"); //	This one is fixed, host 2008 for Mio's birthday 20 Aug and 3 for "Mi" in Mio
+		//ws.onerror = function (err) {
+		//}
+
+		ws.onopen = function (event) {
+			SendReg();
+		};
+        
+        ws.onclose = function (event) {
+			switch (mode){
+				case 1:
+					spn.textContent = "Can't reach server";
+					break;
+				case 3:
+					spn.textContent = "Disconnected";
+					break;
+				case 4:
+					sendBtn = null;
+					ChatText = null;
+					ChatInputPanel = null;
+					break;
+				case 5:
+					ChatItemObserver.disconnect();
+					break;
+				case 6:
+					spn.textContent = "Disconnected";
+					break;
+			}
+			btn.textContent = "Sync MChat";
+			mode = 0;
+        };
+		
+		ws.onmessage = function (event) {
+			MsgNexus(event.data.toString());
+		};
+	
+	} else {
+		alert('NOT IN SYNCING TARGET WEBPAGE');
+	}
+}
+
+function MsgNexus(StringData) {
+	var NexusParse = StringData.toString().match(/\"Act\":\"MChad-RegOK\"|\"Act\":\"MChad-RollCallApp\"|\"Act\":\"MChad-SetMode\"|\"Act\":\"MChad-PlayApp\"|\"Act\":\"MChad-PauseApp\"|\"Act\":\"MChad-TimeSetApp\"|\"Act\":\"MChad-LiveSend\"|\"Act\":\"MChad-RegListener\"|\"Act\":\"MChad-FilterApp\"|\"Act\":\"MChad-PreciseSyncApp\"|\"Act\":\"MChad-PreciseTimeSetApp\"/);
+	
+	if (NexusParse == null){
+		return;
+	}
+
+	if (NexusParse.length != 0){
+		switch(NexusParse[0]){
+			case ("\"Act\":\"MChad-RegOK\""):
+				mode = 2;
+				btn.textContent = "Synced - Idle";
+				break;
+			case ("\"Act\":\"MChad-RollCallApp\""):
+				SendRollCall();
+				break;
+			case ("\"Act\":\"MChad-SetMode\""):
+				var ParsedData = StringData.toString().split("\",\"");
+				if (ParsedData.length != 3){
+					return;
+				}
+		
+				if (ParsedData[1].split("\":\"")[1].replace("\"","") == UID){
+					switch (ParsedData[2].split("\":\"")[1].replace("\"}","")){
+						case ("Archive"):
+							if (mode < 3){
+								mode = 2;
+								btn.textContent = "ARCHIVE MODE UNAVAILABLE";
+							}
+							break;
+						case ("LiveChat"):
+							if (mode < 3){
+								mode = 2;
+								btn.textContent = "BOUNCING NOT AVAILABLE";
+							}
+							break;
+						case ("SyncTL"):
+							if (mode < 3){
+								mode = 2;
+								btn.textContent = "PRECISE SYNC MODE UNAVAILABLE";
+							}
+							break;
+					}
+				}
+				break;
+			case ("\"Act\":\"MChad-TimeSetApp\""):
+				var ParsedData = StringData.toString().split("\",\"");
+				if (ParsedData.length != 3){
+					return;
+				}
+		
+				if ((ParsedData[1].split("\":\"")[1].replace("\"","") == UID) && (mode == 3)){
+					SetTime(ParsedData[2].split("\":\"")[1].replace("\"}",""));
+				}
+				break;
+			case ("\"Act\":\"MChad-LiveSend\""):
+				var ParsedData = StringData.toString().split("\",\"");
+				if (ParsedData.length != 3){
+					return;
+				}
+
+				if ((ParsedData[1].split("\":\"")[1].replace("\"","") == UID) && (mode == 4)){
+					SendTextEnter(ParsedData[2].split("\":\"")[1].replace("\"}",""));
+				}
+				break;
+			case ("\"Act\":\"MChad-RegListener\""):
+				if (mode < 3){
+					mode = 5;
+					btn.textContent = "Synced - Listener (Click to Unsync)";
+					ChatListener();
+				}
+				break;
+			case ("\"Act\":\"MChad-FilterApp\""):
+				var ParsedData = StringData.toString().split("\",\"");
+				if (ParsedData.length != 3){
+					return;
+				}
+				
+				switch (ParsedData[1].split("\":\"")[1].replace("\"","")){
+					case ("TAG"):
+						TagList = ParsedData[2].split("\":\"")[1].replace("\"}","").replaceAll(",", " ");
+						break;
+					case ("KEYWORD"):
+						KeyWordList = ParsedData[2].split("\":\"")[1].replace("\"}","");
+						break;						
+				}
+				spn.textContent = "Synced Filter"
+				break;
+			case ("\"Act\":\"MChad-PreciseTimeSetApp\""):
+				var ParsedData = StringData.toString().split("\",\"");
+				if (ParsedData.length != 4){
+					return;
+				}
+						
+				if ((ParsedData[1].split("\":\"")[1].replace("\"}","") == UID) && (mode == 6)){
+					SetTimePrecise(ParsedData[2].split("\":\"")[1], ParsedData[3].split("\":\"")[1].replace("\"}",""));
+				}				
+				break;
+	
+		}
+	}
+}
+
+function BtnNexus() {
+	spn.textContent = "";
+	if (mode == 0) {
+		btn.textContent = "Syncing..."
+		mode = 1;
+		OpenSync();
+	} else {
+		ws.close();
+	}
+}
+//=============================================================================================================
+
+
+
+var ws;
+var btn = document.createElement('button');
+btn.onclick = BtnNexus;
+btn.textContent = "Sync MChat"
+btn.style.margin = "5px"
+btn.style.background = 'black';
+btn.style.color = 'white';
+btn.style.fontSize = '15px';
+btn.style.cursor = 'pointer';
+btn.style.textAlign = 'center';
+btn.style.borderRadius = '15px';
+btn.style.padding = '8px';
+
+var spn = document.createElement('span');
+spn.textContent = "";
+spn.style.fontSize = '15px';
+spn.style.background = 'black';
+spn.style.color = 'white';
+
+var sendBtn; 
+var ChatText;
+var ListenerTarget;
+var ChatInputPanel;
+
+var mode = 0;
+
+var ChatElementTarget = "chat-messages";
+/*
+	0 : NOT SYNCED
+	1 : SYNCING
+	2 : SYNCED-IDLE
+	3 : SYNCED-ARCHIVE
+	4 : SYNCED-LIVECHAT
+	5 : SYNCED-LISTENER
+*/
+
+function LoadButtons() {
+	var target = document.getElementById(ChatElementTarget);
+	var ExtContainer = document.createElement('div');
+	ExtContainer.id = "Extcontainer";
+	target.prepend(ExtContainer);
+	ExtContainer.appendChild(btn);
+	ExtContainer.appendChild(spn);
+}
+
+async function WaitUntilLoad(){
+	while (true){
+		var target = document.getElementById(ChatElementTarget);
+		if (target.length != 0){
+			if (document.getElementById("Extcontainer") != null){
+				var Extcontainer = document.getElementById("Extcontainer");
+				Extcontainer.parentNode.removeChild(Extcontainer);
+			}
+			LoadButtons();
+			break;
+		} else {
+			console.log("Not loaded yet");
+		}
+		await new Promise(resolve => setTimeout(resolve, 1000));
+	}
+}
+
+WaitUntilLoad();
